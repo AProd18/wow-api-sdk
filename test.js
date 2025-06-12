8a908a0a3e8119f2adfb300bd6b741fa86d00ad5
AProd18 <aleksandar_prod18@hotmail.com>
2025-06-12 13:04:25 +0200

    feat: add getCharacterEquipment function to retrieve currently equipped gear

--- test.js
+++ test.js
@@ -3,25 +3,26 @@ import {
   getCharacterMedia,
   getItemMedia,
   getCharacterSpecializations,
+  getCharacterEquipment,
 } from "./lib/api.js";
 
 const run = async () => {
   const item = await getItem(19019, "en_GB", "eu");
   console.log("Item:", item.name);
 
-  const character = await getCharacterProfile("eu", "Kazzak", "Qtxoxo");
+  const character = await getCharacterProfile("eu", "Sylvanas", "Scartx");
   console.log("Character:", character.name);
 
   const itemMedia = await getItemMedia("eu", 19019); // Thunderfury
   console.log("Item Media URL:", itemMedia.assets?.[0]?.value);
 
-  const characterMedia = await getCharacterMedia("eu", "Kazzak", "Qtxoxo");
+  const characterMedia = await getCharacterMedia("eu", "Sylvanas", "Scartx");
   console.log("Character Media URL:", characterMedia.assets?.[0]?.value);
 
   const specializations = await getCharacterSpecializations(
     "eu",
-    "Kazzak",
-    "Qtxoxo"
+    "Sylvanas",
+    "Scartx"
   );
   // console.dir(specializations, { depth: null });
 
@@ -32,6 +33,17 @@ const run = async () => {
     );
   } else {
     console.log("Active Specialization: No active spec");
+  }
+
+  const equipment = await getCharacterEquipment("eu", "Sylvanas", "Scartx");
+
+  if (equipment && equipment.equipped_items) {
+    console.log("Equipped items:");
+    equipment.equipped_items.forEach((item) => {
+      console.log(`- ${item.slot.type}: ${item.name}`);
+    });
+  } else {
+    console.log("No equipment found.");
   }
 };
 
