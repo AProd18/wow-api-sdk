60dddf5b96f706cbbc6021eefe71daecd020bd5c
AProd18 <aleksandar_prod18@hotmail.com>
2025-06-20 18:01:01 +0200

    feat: add all import and export functions in index.js

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ import {
   getItem,
   getCharacterProfile,
   getCharacterSpecializations,
+  getCharacterMedia,
+  getCharacterAchievements,
+  getCharacterMounts,
+  getCharacterTitles,
+  getCharacterPets,
+  getItemMedia,
+  getCharacterEquipment,
 } from "./lib/api.js";
 
-export { getItem, getCharacterProfile, getCharacterSpecializations };
+export {
+  getItem,
+  getCharacterProfile,
+  getCharacterSpecializations,
+  getCharacterMedia,
+  getCharacterAchievements,
+  getCharacterMounts,
+  getCharacterTitles,
+  getCharacterPets,
+  getItemMedia,
+  getCharacterEquipment,
+};
